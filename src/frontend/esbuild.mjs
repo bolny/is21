954aeb9b77890a7854940a3d734ad3740eaae9e4
Brendon O'Laney <brendon@brendonolaney.com>
2024-03-22 15:30:11 -0700

    Add basic react toolchain

---
+++ src/frontend/esbuild.mjs
@@ -0,0 +1,11 @@
+import * as esbuild from "esbuild";
+
+await esbuild.build({
+    entryPoints: ["./is21/index.tsx"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    target: ["chrome109", "firefox102", "safari15", "edge114"],
+    outfile: "./build/index.js",
+    loader: { ".tsx": "tsx" },
+});
