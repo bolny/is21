49d8712fe00232e2ce33e948375bc9e8dff69eba
Brendon O'Laney <brendon@brendonolaney.com>
2024-03-23 12:43:20 -0700

    Switch to using sqlite3 for expediency

--- src/backend/is21/settings.py
+++ src/backend/is21/settings.py
@@ -62,13 +62,8 @@ WSGI_APPLICATION = 'is21.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'NAME': 'postgres',
-        'USER': 'bolny',
-        'OPTIONS': {
-            "passfile": ".pgpass",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
