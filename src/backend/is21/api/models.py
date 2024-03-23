ab055887b693c54986886d75123d3de243eb1bce
Brendon O'Laney <brendon@brendonolaney.com>
2024-03-22 19:40:59 -0700

    Add amount field to paint.

--- src/backend/is21/api/models.py
+++ src/backend/is21/api/models.py
@@ -9,4 +9,8 @@ class Lane(models.Model):
 class Paint(models.Model):
     lane = models.ForeignKey(Lane, on_delete=models.PROTECT)
     name = models.CharField(max_length=255)
-    colour = models.CharField(max_length=100)
+    colour = models.CharField(
+        max_length=100,
+        verbose_name="Color in hex format, ie. '#a1a1a1'"
+    )
+    amount = models.IntegerField(verbose_name="Amount in liters", default=0)
