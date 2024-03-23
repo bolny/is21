1bb6be6e55544634914a9c6e4ff9c57373ab9a9b
Brendon O'Laney <brendon@brendonolaney.com>
2024-03-23 16:48:33 -0700

    Add Paint editing

--- src/frontend/is21/index.jsx
+++ src/frontend/is21/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import axios from 'axios'
@@ -15,25 +15,101 @@ const getLanes = async () => {
 }
 
 const getPaint = async () => {
-    const response = await axios.get(apiRoot +"paint/", requestOptions);
+    const response = await axios.get(apiRoot + "paint/", requestOptions);
     return response;
 }
 
-function Paint({paint}) {
+const putPaint = (id, paintData) => {
+    axios.put(apiRoot + "paint/" + id + "/", paintData, requestOptions)
+        .then(() => {
+            queryClient.invalidateQueries();
+        })
+        .catch(() => {
+            alert("There was an error connecting to the server. Try again.")
+        });
+}
+
+function PaintEditForm({paint, laneData, showForm}) {
+    const [newAmount, setNewAmount] = useState(false)
+    const [newLane, setNewLane] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const paintData = {
+            lane: newLane || paint.lane,
+            name: paint.name,
+            colour: paint.colour,
+            text_colour: paint.text_colour,
+            amount: newAmount || paint.amount
+        }
+
+        putPaint(paint.id, paintData);
+
+        // Toggles form visibility to off.
+        showForm();
+    }
+
     return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Amount:
+                <input
+                    type="text"
+                    defaultValue={paint.amount}
+                    onChange={(e) => setNewAmount(e.target.value)}
+                />
+            </label>
+            <LaneSelectForm paint={paint} laneData={laneData} setNewLane={setNewLane} />
+            <input type="submit" />
+        </form>
+    )
+}
+
+function LaneSelectForm({paint, laneData, setNewLane}) {
+    const options = laneData.map((lane) => {
+        return (
+            <option key={lane.id} value={lane.id}>{lane.name}</option>
+        )
+    });
+    return (
+        <select
+            name="Lane"
+            defaultValue={paint.lane}
+            onChange={(e) => setNewLane(e.target.value)}
+        >
+            {options}
+        </select>
+    )
+}
+
+
+function Paint({paint, laneData}) {
+    const [shouldShowForm, setShouldShowForm] = useState(false);
+
+    const showForm = () => {
+      setShouldShowForm(!shouldShowForm);
+    }
+
+    return (
+        <>
         <div className='grid-x'
-            style={{background: paint.colour}}
+            style={{background: paint.colour, color: paint.text_colour}}
         >
             <div className='cell small-3'>
                 {paint.name}
             </div>
             <div className='cell small-3'>
-                {paint.amount}L
+                {paint.amount}
             </div>
             <div className='cell small-3'>
-                <button className='button'>Edit</button>
+                <button className='button' onClick={showForm}>Edit</button>
             </div>
         </div>
+        {shouldShowForm && (
+            <PaintEditForm paint={paint} laneData={laneData} showForm={showForm}/>
+        )}
+        </>
     )
 }
 
@@ -57,7 +133,7 @@ function Lanes({laneData, paintData}) {
 
         const paints = paintsForLaneData.map((paint) => {
             return (
-                <Paint key={paint.id} paint={paint} />
+                <Paint key={paint.id} paint={paint} laneData={laneData} />
             );
         });
         return (
