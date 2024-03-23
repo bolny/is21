a8afbaf6813eb3694aa4afa9d13d0b9021e2bc85
Brendon O'Laney <brendon@brendonolaney.com>
2024-03-22 19:23:33 -0700

    Add models and database migration

--- src/backend/is21/api/models.py
+++ src/backend/is21/api/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+
+class Lane(models.Model):
+    name = models.CharField(max_length=255)
+
+
+
+class Paint(models.Model):
+    lane = models.ForeignKey(Lane, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    colour = models.CharField(max_length=100)
